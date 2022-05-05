3fcec718608a2091d69b33c455c9d85e60d6abfb
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-05 10:16:30 -0300

    removing unnecessary files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | MyShopping</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
