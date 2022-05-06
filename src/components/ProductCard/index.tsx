3c31c64ddd336b99a272ccc6379426a92d9cad77
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-06 12:45:02 -0300

    styling product card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,3 +1,32 @@
+import Image from "next/image"
+import styles from "./styles.module.scss"
+
+
 export function ProductCard() {
-    return <></>
+    return (
+        <div className={styles.container}>
+            <Image
+                priority
+                src="/images/terno.jpg"
+                alt="Terno"
+                width={100}
+                height={320}
+            />
+            <h3>Terno Xadrez</h3>
+
+            <hr />
+            <div className={styles.content}>
+                <p>R$400,00</p>
+                
+                <div className={styles.favoriteContainer}>
+                    <Image
+                        src="/images/heart.svg"
+                        alt="Ícone de coração"
+                        width={24}
+                        height={24}
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head'
-
+import { ProductCard } from '../components/ProductCard'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Home | MyShopping</title>
       </Head>
-    </>
+
+      <div className={styles.productContainer}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
   )
 }
