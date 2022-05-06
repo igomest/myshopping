541889154bd7be6e007f5b02e266d942099e4768
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-06 10:52:57 -0300

    styling header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,31 @@
+import styles from "./styles.module.scss"
+import Image from "next/image"
+import Link from "next/link"
+
+
 export function Header() {
-    return <></>
+    return (
+        <header className={styles.container}>
+            <div className={styles.logoContainer}>
+                <Image
+                    src="/images/cart.svg"
+                    alt="Logo"
+                    height={35}
+                    width={35}
+                />
+                <h2>MYSHOPPING</h2>
+            </div>
+
+            <Link href="/favorites">
+                <div className={styles.bagContainer}>
+                    <Image
+                        src="/images/bag.svg"
+                        alt="Sacola"
+                        height={24}
+                        width={24}
+                    />
+                </div>
+            </Link>
+        </header>
+    )
 }
